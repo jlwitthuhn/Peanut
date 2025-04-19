edea1508988b4d408aaaf8384b750da6fd667f0e
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-04-18 23:58:14 -0700

    Remove leftover import

--- src/main/java/io/github/jlwitthuhn/peanut/controller/SetupController.java
+++ src/main/java/io/github/jlwitthuhn/peanut/controller/SetupController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-
 @Controller
 @RequestMapping("/setup")
 public class SetupController

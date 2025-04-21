299e36b22a4ba9eb4395478a59dd1b5fe3478905
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-04-21 00:58:27 -0700

    Add basic sanity test

--- src/test/java/io/github/jlwitthuhn/peanut/PeanutApplicationTests.java
+++ src/test/java/io/github/jlwitthuhn/peanut/PeanutApplicationTests.java
@@ -1,13 +1,26 @@
 package io.github.jlwitthuhn.peanut;
 
+import io.github.jlwitthuhn.peanut.controller.RootController;
+import io.github.jlwitthuhn.peanut.controller.SetupController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-class PeanutApplicationTests {
+class PeanutApplicationTests
+{
+	@Autowired
+	private RootController rootController;
+
+	@Autowired
+	private SetupController setupController;
 
 	@Test
-	void contextLoads() {
+	void controllersLoad()
+	{
+		assertThat(rootController).isNotNull();
+		assertThat(setupController).isNotNull();
 	}
-
 }

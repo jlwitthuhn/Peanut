0ec0845550437fa5263f517ca6f203750f1818d1
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-05-09 22:42:14 -0700

    Add extra logging to ErrorControllerImpl for when a status has no error code

--- src/main/java/io/github/jlwitthuhn/peanut/controller/ErrorControllerImpl.java
+++ src/main/java/io/github/jlwitthuhn/peanut/controller/ErrorControllerImpl.java
@@ -7,6 +7,8 @@ package io.github.jlwitthuhn.peanut.controller;
 import io.github.jlwitthuhn.peanut.util.ViewShortcuts;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -18,12 +20,19 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/error")
 public class ErrorControllerImpl implements ErrorController
 {
+	private static final Logger logger = LoggerFactory.getLogger(ErrorControllerImpl.class);
+
 	@GetMapping
 	public ModelAndView error(HttpServletRequest request)
 	{
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		if (!(status instanceof Integer statusInt))
 		{
+			logger.error("Caught error with non-Integer status: {}", status);
+			if (status != null)
+			{
+				logger.error("Status class is {}", status.getClass());
+			}
 			return ViewShortcuts.simpleMessage("Error", "An error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		final String title = String.format("Error - HTTP %d", statusInt);

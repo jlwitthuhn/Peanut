2cca742208e36ea6a7980fd8b063f23f4441692d
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-05-13 00:10:41 -0700

    Fix logged in status not being reflected on ExceptionResolver pages

--- src/main/java/io/github/jlwitthuhn/peanut/controller/ExceptionResolver.java
+++ src/main/java/io/github/jlwitthuhn/peanut/controller/ExceptionResolver.java
@@ -5,9 +5,11 @@
 package io.github.jlwitthuhn.peanut.controller;
 
 import io.github.jlwitthuhn.peanut.err.DBNotInitializedException;
+import io.github.jlwitthuhn.peanut.interceptor.LoggedInUserInterceptor;
 import io.github.jlwitthuhn.peanut.util.ViewShortcuts;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -18,32 +20,46 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 
 @Component
+@RequiredArgsConstructor
 public class ExceptionResolver extends AbstractHandlerExceptionResolver
 {
+	private final LoggedInUserInterceptor loggedInUserInterceptor;
+
 	private static final Logger logger = LoggerFactory.getLogger(ExceptionResolver.class);
 
 	@Override
 	protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 	{
+		ModelAndView result = null;
 		switch (ex) {
 			case BadSqlGrammarException badSqlGrammarException ->
 			{
 				logger.error("Caught BadSqlGrammarException in ExceptionResolver", ex);
-				return ViewShortcuts.simpleMessage("SQL Error", "Failed to execute SQL query.", HttpStatus.INTERNAL_SERVER_ERROR);
+				result = ViewShortcuts.simpleMessage("SQL Error", "Failed to execute SQL query.", HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 			case CannotGetJdbcConnectionException cannotGetJdbcConnectionException ->
 			{
-				return ViewShortcuts.simpleMessage("DB Error", "Unable to connect to the database.", HttpStatus.INTERNAL_SERVER_ERROR);
+				result = ViewShortcuts.simpleMessage("DB Error", "Unable to connect to the database.", HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 			case DBNotInitializedException DBNotInitializedException ->
 			{
-				return ViewShortcuts.simpleMessage("DB Error", "Database has not been initialized.", HttpStatus.INTERNAL_SERVER_ERROR);
+				result = ViewShortcuts.simpleMessage("DB Error", "Database has not been initialized.", HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 			default ->
 			{
 				logger.error("Caught unknown exception in ExceptionResolver", ex);
-				return ViewShortcuts.simpleMessage("Error", "Unknown error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
+				result = ViewShortcuts.simpleMessage("Error", "Unknown error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 		}
+
+		try
+		{
+			loggedInUserInterceptor.postHandle(request, response, handler, result);
+		}
+		catch (Exception exInner)
+		{
+			logger.error("Caught unknown exception while fetching login info", exInner);
+		}
+		return result;
 	}
 }

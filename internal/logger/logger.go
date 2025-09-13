5d76a3edca020bdbc6dec4bfde11bf1aef237ac2
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-09-12 23:22:56 -0700

    Only use one logger per logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -5,28 +5,29 @@ import (
 	"os"
 )
 
-var traceLogger = log.New(os.Stdout, "[TRAC] ", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
-var infoLogger = log.New(os.Stdout, "[INFO] ", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
-var warnLogger = log.New(os.Stdout, "[WARN] ", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
-var errorLogger = log.New(os.Stdout, "[EROR] ", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
-var fatalLogger = log.New(os.Stdout, "[!!!!] ", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
+var theLogger = log.New(os.Stdout, "[TRAC] ", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
 
 func Trace(args ...any) {
-	traceLogger.Println(args...)
+	fullArgs := append([]any{"[TRAC]"}, args...)
+	theLogger.Println(fullArgs...)
 }
 
 func Info(args ...any) {
-	infoLogger.Println(args...)
+	fullArgs := append([]any{"[INFO]"}, args...)
+	theLogger.Println(fullArgs...)
 }
 
 func Warn(args ...any) {
-	warnLogger.Println(args...)
+	fullArgs := append([]any{"[WARN]"}, args...)
+	theLogger.Println(fullArgs...)
 }
 
 func Error(args ...any) {
-	errorLogger.Println(args...)
+	fullArgs := append([]any{"[EROR]"}, args...)
+	theLogger.Println(fullArgs...)
 }
 
 func Fatal(args ...any) {
-	fatalLogger.Fatal(args...)
+	fullArgs := append([]any{"[!!!!]"}, args...)
+	theLogger.Fatal(fullArgs...)
 }

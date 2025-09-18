951b73a137aeb4a91ecbcbc673d9656a8abf71bd
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-09-17 18:41:01 -0700

    Clean up middleware.go

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -4,15 +4,19 @@
 
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"slices"
+)
 
 type MiddlewareFunc func(next http.Handler) http.Handler
 
 func WrapHandler(baseHandler http.Handler, middleware ...MiddlewareFunc) http.Handler {
-	var result http.Handler = baseHandler
-	// Walk through the list backwards so the order the handlers run matches the list
-	for i := len(middleware) - 1; i >= 0; i-- {
-		result = middleware[i](result)
+	result := baseHandler
+	// The first middleware wraps the second, which wraps the third, etc
+	// Because of this, we need to build the final handler starting at the inner layer and working outwards
+	for _, thisMiddleware := range slices.Backward(middleware) {
+		result = thisMiddleware(result)
 	}
 	return result
 }

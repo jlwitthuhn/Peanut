772470026600e7ea4e513a4efe966282964a22a9
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-09-17 19:19:55 -0700

    Move DB connection logic to separate package

---
+++ internal/persistence/postgres.go
@@ -0,0 +1,51 @@
+// This file is part of Peanut and is licensed under the AGPLv3
+// https://www.gnu.org/licenses/agpl-3.0.en.html
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package persistence
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"peanut/internal/logger"
+)
+
+func ConnectToPostgres() *sql.DB {
+	var host string
+	if os.Getenv("PEANUT_DB_HOST") != "" {
+		host = os.Getenv("PEANUT_DB_HOST")
+	} else {
+		host = "localhost"
+	}
+	var user string
+	if os.Getenv("PEANUT_DB_USER") != "" {
+		user = os.Getenv("PEANUT_DB_USER")
+	} else {
+		user = "peanut"
+	}
+	var password string
+	if os.Getenv("PEANUT_DB_PASSWORD") != "" {
+		password = os.Getenv("PEANUT_DB_PASSWORD")
+	} else {
+		password = "password"
+	}
+	var dbname string
+	if os.Getenv("PEANUT_DB_NAME") != "" {
+		dbname = os.Getenv("PEANUT_DB_NAME")
+	} else {
+		dbname = "peanut"
+	}
+
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		logger.Fatal("Error connecting to database:", err)
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		logger.Warn("Database did not respond to ping:", pingErr)
+	}
+
+	return db
+}
--- main.go
+++ main.go
@@ -5,14 +5,12 @@
 package main
 
 import (
-	"database/sql"
 	"embed"
-	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
-	"os"
 	"peanut/internal/middleware"
+	"peanut/internal/persistence"
 
 	_ "github.com/lib/pq"
 
@@ -26,45 +24,6 @@ var staticFs embed.FS
 
 //go:embed template
 var templateFs embed.FS
-
-func connectDb() *sql.DB {
-	var host string
-	if os.Getenv("PEANUT_DB_HOST") != "" {
-		host = os.Getenv("PEANUT_DB_HOST")
-	} else {
-		host = "localhost"
-	}
-	var user string
-	if os.Getenv("PEANUT_DB_USER") != "" {
-		user = os.Getenv("PEANUT_DB_USER")
-	} else {
-		user = "peanut"
-	}
-	var password string
-	if os.Getenv("PEANUT_DB_PASSWORD") != "" {
-		password = os.Getenv("PEANUT_DB_PASSWORD")
-	} else {
-		password = "password"
-	}
-	var dbname string
-	if os.Getenv("PEANUT_DB_NAME") != "" {
-		dbname = os.Getenv("PEANUT_DB_NAME")
-	} else {
-		dbname = "peanut"
-	}
-
-	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user, password, dbname)
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		logger.Fatal("Error connecting to database:", err)
-	}
-	pingErr := db.Ping()
-	if pingErr != nil {
-		logger.Warn("Error pinging database:", pingErr)
-	}
-
-	return db
-}
 
 func main() {
 	logger.Info("++ Starting Peanut ++")
@@ -85,8 +44,8 @@ func main() {
 	pages.RegisterIndexHandlers(mux)
 	muxHandler := middleware.WrapHandler(mux, middleware.RequestTimer, middleware.RequestLog)
 
-	logger.Info("Connecting to database...")
-	connectDb()
+	logger.Info("Connecting to postgres...")
+	persistence.ConnectToPostgres()
 
 	logger.Info("Startup complete, listening on :8080")
 	logger.Fatal(http.ListenAndServe(":8080", muxHandler))

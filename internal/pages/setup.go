c74ef0f57ecb3035a6ae7b7776ba04a1d4b180fc
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-09-19 18:39:12 -0700

    Validate inputs on setup page

--- internal/pages/setup.go
+++ internal/pages/setup.go
@@ -8,8 +8,22 @@ import (
 	"net/http"
 	"peanut/internal/logger"
 	"peanut/internal/middleutil"
+	"peanut/internal/pages/genericpage"
+	"peanut/internal/service/db_service"
 	"peanut/internal/template"
 )
+
+func isEmailValid(email string) bool {
+	return len(email) > 1
+}
+
+func isPasswordValid(password string) bool {
+	return len(password) > 1
+}
+
+func isUsernameValid(username string) bool {
+	return len(username) > 1
+}
 
 func RegisterSetupHandlers(mux *http.ServeMux) {
 
@@ -24,4 +38,48 @@ func RegisterSetupHandlers(mux *http.ServeMux) {
 		}
 	})
 	mux.Handle("GET /setup", getSetupHandler)
+
+	postSetupHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		configTableExists, configTableErr := db_service.DoesTableExist("config_int")
+		if configTableErr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			genericpage.RenderSimpleMessage("Error", "Failed to query database.", w, r)
+			return
+		}
+		if configTableExists {
+			w.WriteHeader(http.StatusConflict)
+			genericpage.RenderSimpleMessage("Error", "Database has already been initialized.", w, r)
+			return
+		}
+
+		adminName := r.PostFormValue("admin-name")
+		email := r.PostFormValue("email")
+		adminPassword := r.PostFormValue("admin-pass")
+		adminPassword2 := r.PostFormValue("admin-pass-2")
+
+		if !isUsernameValid(adminName) {
+			w.WriteHeader(http.StatusBadRequest)
+			genericpage.RenderSimpleMessage("Error", "Username is not valid.", w, r)
+			return
+		}
+		if !isEmailValid(email) {
+			w.WriteHeader(http.StatusBadRequest)
+			genericpage.RenderSimpleMessage("Error", "Email is not valid.", w, r)
+			return
+		}
+		if adminPassword != adminPassword2 {
+			w.WriteHeader(http.StatusBadRequest)
+			genericpage.RenderSimpleMessage("Error", "Passwords must match.", w, r)
+			return
+		}
+		if !isPasswordValid(adminPassword) {
+			w.WriteHeader(http.StatusBadRequest)
+			genericpage.RenderSimpleMessage("Error", "Passwords is not valid.", w, r)
+			return
+		}
+
+		// TODO: This should create DB tables with default data through `SetupService`
+		genericpage.RenderSimpleMessage("TODO", "This is the part where database tables would be created.", w, r)
+	})
+	mux.Handle("POST /setup", postSetupHandler)
 }

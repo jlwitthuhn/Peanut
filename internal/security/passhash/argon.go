fe4b198b58b49adf2cdd46ab28d7183d6de99a62
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-09-21 21:25:29 -0700

    Use random data as password salt

--- internal/security/passhash/argon.go
+++ internal/security/passhash/argon.go
@@ -5,23 +5,33 @@
 package passhash
 
 import (
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 
 	"golang.org/x/crypto/argon2"
 )
 
-func EncodeArgon2IdPhcString(plaintext string, salt string, memory uint32, time uint32, parallel uint8) string {
-	saltB64 := base64.StdEncoding.EncodeToString([]byte(salt))
-	bytes := argon2.IDKey([]byte(plaintext), []byte(salt), time, memory, parallel, 32)
+func EncodeArgon2IdPhcString(plaintext string, salt []byte, memory uint32, time uint32, parallel uint8) string {
+	saltB64 := base64.StdEncoding.EncodeToString(salt)
+	bytes := argon2.IDKey([]byte(plaintext), salt, time, memory, parallel, 32)
 	b64 := base64.StdEncoding.EncodeToString(bytes)
 	return fmt.Sprintf("$argon2id$v=19$m=%d,t=%d,p=%d$%s$%s", memory, time, parallel, saltB64, b64)
 }
 
 func EncodeDefaultArgon2IdPhcString(plaintext string) string {
-	salt := "salt"
+	salt := genSalt()
 	var memory uint32 = 24 * 1024
 	var time uint32 = 2
 	var parallel uint8 = 1
 	return EncodeArgon2IdPhcString(plaintext, salt, memory, time, parallel)
+}
+
+func genSalt() []byte {
+	bytes := make([]byte, 8)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
 }

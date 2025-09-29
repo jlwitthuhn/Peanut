7cfab3c86c0298efe72e974f7fa2c139d8b5a90a
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-09-29 02:41:07 -0700

    Make logger include caller filename and line number

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -9,41 +9,52 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"peanut/internal/middleutil"
+	"runtime"
 )
 
 var theLogger = log.New(os.Stdout, "", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
 
 func Trace(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[TRAC]"}, args...)
+	fullArgs := append([]any{formatRequestId(r), "[TRAC]", getCallerLocation()}, args...)
 	theLogger.Println(fullArgs...)
 }
 
 func Debug(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[DBUG]"}, args...)
+	fullArgs := append([]any{formatRequestId(r), "[DBUG]", getCallerLocation()}, args...)
 	theLogger.Println(fullArgs...)
 }
 
 func Info(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[INFO]"}, args...)
+	fullArgs := append([]any{formatRequestId(r), "[INFO]", getCallerLocation()}, args...)
 	theLogger.Println(fullArgs...)
 }
 
 func Warn(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[WARN]"}, args...)
+	fullArgs := append([]any{formatRequestId(r), "[WARN]", getCallerLocation()}, args...)
 	theLogger.Println(fullArgs...)
 }
 
 func Error(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[EROR]"}, args...)
+	fullArgs := append([]any{formatRequestId(r), "[EROR]", getCallerLocation()}, args...)
 	theLogger.Println(fullArgs...)
 }
 
 func Fatal(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[!!!!]"}, args...)
+	fullArgs := append([]any{formatRequestId(r), "[!!!!]", getCallerLocation()}, args...)
 	theLogger.Fatal(fullArgs...)
 }
 
 func formatRequestId(r *http.Request) string {
 	return fmt.Sprintf("(%s)", middleutil.RetrieveRequestId(r))
+}
+
+func getCallerLocation() string {
+	// 0 is getCallerLocation
+	// 1 is Info, Warn, Error, etc
+	// 2 is the code that called into this package
+	_, file, line, _ := runtime.Caller(2)
+	fileName := filepath.Base(file)
+	return fmt.Sprintf("%s:%d:", fileName, line)
 }

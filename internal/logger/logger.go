3c2aca90436f46c8850add404c3a0e02a426c8ca
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-01-31 03:05:18 -0800

    Add support for setting log level through environment variable

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -14,36 +14,72 @@ import (
 	"runtime"
 )
 
+func getLogLevel() int {
+	envLogLevel := os.Getenv("PEANUT_LOG_LEVEL")
+	switch envLogLevel {
+	case "fatal":
+		return 0
+	case "error":
+		return 1
+	case "warn":
+		return 2
+	case "info":
+		return 3
+	case "debug":
+		return 4
+	case "trace":
+		return 5
+	default:
+		return 5
+	}
+}
+
 var theLogger = log.New(os.Stdout, "", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
+var logLevel = getLogLevel()
 
 func Trace(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[TRAC]", getCallerLocation()}, args...)
-	theLogger.Println(fullArgs...)
+	if logLevel >= 5 {
+		fullArgs := append([]any{formatRequestId(r), "[TRAC]", getCallerLocation()}, args...)
+		theLogger.Println(fullArgs...)
+	}
 }
 
 func Debug(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[DBUG]", getCallerLocation()}, args...)
-	theLogger.Println(fullArgs...)
+	if logLevel >= 4 {
+		fullArgs := append([]any{formatRequestId(r), "[DBUG]", getCallerLocation()}, args...)
+		theLogger.Println(fullArgs...)
+	}
 }
 
 func Info(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[INFO]", getCallerLocation()}, args...)
-	theLogger.Println(fullArgs...)
+	if logLevel >= 3 {
+		fullArgs := append([]any{formatRequestId(r), "[INFO]", getCallerLocation()}, args...)
+		theLogger.Println(fullArgs...)
+	}
 }
 
 func Warn(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[WARN]", getCallerLocation()}, args...)
-	theLogger.Println(fullArgs...)
+	if logLevel >= 2 {
+		fullArgs := append([]any{formatRequestId(r), "[WARN]", getCallerLocation()}, args...)
+		theLogger.Println(fullArgs...)
+	}
 }
 
 func Error(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[EROR]", getCallerLocation()}, args...)
-	theLogger.Println(fullArgs...)
+	if logLevel >= 1 {
+		fullArgs := append([]any{formatRequestId(r), "[EROR]", getCallerLocation()}, args...)
+		theLogger.Println(fullArgs...)
+	}
 }
 
 func Fatal(r *http.Request, args ...any) {
-	fullArgs := append([]any{formatRequestId(r), "[!!!!]", getCallerLocation()}, args...)
-	theLogger.Fatal(fullArgs...)
+	if logLevel >= 0 {
+		fullArgs := append([]any{formatRequestId(r), "[!!!!]", getCallerLocation()}, args...)
+		theLogger.Fatal(fullArgs...)
+	} else {
+		// End process if logging is disabled
+		os.Exit(1)
+	}
 }
 
 func formatRequestId(r *http.Request) string {
